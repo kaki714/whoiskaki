a4fd9c73737b7c4c751b3ae9523fb9c4bfd0a6e5
kaki714 <123768052+kaki714@users.noreply.github.com>
2023-11-09 13:00:22 +0100

    Update inicio.php

--- www/inicio.php
+++ www/inicio.php
@@ -1,11 +1,19 @@
 <?php
-// Inicia la sesión
-session_start();
+// Verifica si se han enviado datos del formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtiene los datos del formulario
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-// Destruye todas las variables de sesión
-session_destroy();
-
-// Redirige a la página de inicio de sesión
-header("Location: index.html");
-exit();
+    // Verifica las credenciales (esto debe ser reemplazado con una autenticación segura)
+    if ($username === "usuario" && $password === "contraseña") {
+        // Redirige a la página de inicio después de iniciar sesión exitosamente
+        header("Location: inicio.php");
+        exit();
+    } else {
+        // Si las credenciales son incorrectas, redirige de nuevo al formulario de login
+        header("Location: index.html");
+        exit();
+    }
+}
 ?>
